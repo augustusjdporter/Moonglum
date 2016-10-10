4e9fd31f1a424f00dc6ccf8b6c2a5697b63df40a
augustusjdporter <augustusjdporter@gmail.com>
2016-10-10 08:30:24 +0100

    syncing commit

--- planetary-simulation/plot-planetary-simulation.py
+++ planetary-simulation/plot-planetary-simulation.py
@@ -4,7 +4,7 @@ import sys
 
 import matplotlib
 # Force matplotlib to not use any Xwindows backend.
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 
 import numpy as np
 import matplotlib.pyplot as plt
